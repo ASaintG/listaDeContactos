a5fdd538b141a74a87562c6b3c97becdd43dab43
ASaintG <78000780+ASaintG@users.noreply.github.com>
2023-09-25 17:57:15 -0600

    Primer commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const listaContactos=[
+{
+    nombre:"Pedro",
+    apellido:"Benitez",
+    direccion:{
+        ciudad:"Trujillo",
+        direccion:"Colon"
+    },
+    telefono:212313123,
+    correo:"correodePrueb@gamil.com",
+    
+},
+{
+    nombre:"Carlos",
+    apellido:"Smith",
+    direccion:{
+        ciudad:"Trujillo",
+        direccion:"Colon"
+    },
+    telefono:2313112313,
+    correo:"correodePrueb@gamil.com",
+},
+{
+    nombre:"Maria",
+    apellido:"Cardona",
+    direccion:{
+        ciudad:"Trujillo",
+        direccion:"Colon"
+    },
+    telefono:212313,
+    correo:"correodePrueb@gamil.com",
+}];
+
+console.log(listaContactos);
